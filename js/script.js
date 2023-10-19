5e5cf89c2e38e43ec74beb13ebe4da4364cd7f94
Johnatan Rodrigues <johndriguess@gmail.com>
2023-10-18 22:59:43 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -50,61 +50,72 @@ const verificarMega = async (pokemon) => {
 }
 
 const renderPokemon = async (pokemon) => {
-    const data = await fetchPokemon(pokemon);
-    if(data){
-        buttonVoltar.style.display = 'none';
-        mega = await verificarMega(pokemon)
-        pokemonImage.style.display = 'block';
-        searchPokemon = data.id;
-        if(searchPokemon < 650){
-            pokemonNumber.innerHTML = data.id;
-            pokemonName.innerHTML = data.name.split("-").join(" ");
-            pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
-        }else if(searchPokemon > 649 && searchPokemon < 1011){
-            pokemonNumber.innerHTML = data.id;
-            pokemonName.innerHTML = data.name.split("-").join(" ");
-            pokemonImage.src = data['sprites']['front_default']
-        }else if(searchPokemon > 10000){
-            pokemonNumber.innerHTML = "MEGA";
-            pokemonName.innerHTML = data.name.split("-").join(" ");
-            pokemonImage.src = data['sprites']['front_default']
-            buttonVoltar.style.display = 'block';
-            searchPokemon = pokemonOriginal
-        }
-        else{
+    pokemonName.innerHTML = "Carregando...";
+    pokemonImage.style.display = 'none';
+    pokemonNumber.innerHTML = "";
+    pokemonType1.src = "./images/types/blank.png";
+    pokemonType2.src = "./images/types/blank.png";
+    try {
+        const data = await fetchPokemon(pokemon);
+
+        if(data){
+            buttonVoltar.style.display = 'none';
+            mega = await verificarMega(pokemon)
+            pokemonImage.style.display = 'block';
+            searchPokemon = data.id;
+            if(searchPokemon < 650){
+                pokemonNumber.innerHTML = data.id;
+                pokemonName.innerHTML = data.name.split("-").join(" ");
+                pokemonImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+            }else if(searchPokemon > 649 && searchPokemon < 1011){
+                pokemonNumber.innerHTML = data.id;
+                pokemonName.innerHTML = data.name.split("-").join(" ");
+                pokemonImage.src = data['sprites']['front_default']
+            }else if(searchPokemon > 10000){
+                pokemonNumber.innerHTML = "MEGA";
+                pokemonName.innerHTML = data.name.split("-").join(" ");
+                pokemonImage.src = data['sprites']['front_default']
+                buttonVoltar.style.display = 'block';
+                searchPokemon = pokemonOriginal
+            }
+            else{
+                pokemonImage.style.display = 'none';
+                pokemonImage.style.display = 'none';
+                pokemonNumber.innerHTML = data.id;
+                pokemonName.innerHTML = data.name;
+            }
+            if(data['types']['length'] == 1 ){
+                pokemonType2.style.display = 'none';
+                pokemonType1.style.left = "36%";
+                const tipo1 = data['types']['0']['type']['name']
+                pokemonType1.src = "./images/types/"+tipo1+"_type.png"
+            }
+            else{
+                pokemonType2.style.display = null;
+                pokemonType1.style.left = null;
+                let tipo1 = data['types']['0']['type']['name']
+                pokemonType1.src = "./images/types/"+tipo1+"_type.png"
+                let tipo2 = data['types']['1']['type']['name']
+                pokemonType2.src = "./images/types/"+tipo2+"_type.png"
+            }
+            
+        } else{
+            audioNotFound.play()
             pokemonImage.style.display = 'none';
-            pokemonImage.style.display = 'none';
-            pokemonNumber.innerHTML = data.id;
-            pokemonName.innerHTML = data.name;
+            pokemonName.innerHTML = "Não foi encontrado";
+            pokemonNumber.innerHTML = "";
+            pokemonType1.src = "./images/types/blank.png";
+            pokemonType2.src = "./images/types/blank.png";
+            setTimeout(()=> {
+                searchPokemon = 1
+                renderPokemon(searchPokemon);
+            }, 1500)
+            
         }
-        if(data['types']['length'] == 1 ){
-            pokemonType2.style.display = 'none';
-            pokemonType1.style.left = "36%";
-            const tipo1 = data['types']['0']['type']['name']
-            pokemonType1.src = "./images/types/"+tipo1+"_type.png"
-        }
-        else{
-            pokemonType2.style.display = null;
-            pokemonType1.style.left = null;
-            let tipo1 = data['types']['0']['type']['name']
-            pokemonType1.src = "./images/types/"+tipo1+"_type.png"
-            let tipo2 = data['types']['1']['type']['name']
-            pokemonType2.src = "./images/types/"+tipo2+"_type.png"
-        }
-        
-    } else{
-        audioNotFound.play()
-        pokemonImage.style.display = 'none';
-        pokemonName.innerHTML = "Não foi encontrado";
-        pokemonNumber.innerHTML = "";
-        pokemonType1.src = "./images/types/blank.png";
-        pokemonType2.src = "./images/types/blank.png";
-        setTimeout(()=> {
-            searchPokemon = 1
-            renderPokemon(searchPokemon);
-        }, 1500)
-        
+    }catch (error) {
+        console.error("Erro ao buscar o Pokémon:", error);
     }
+    
     
 }
 
